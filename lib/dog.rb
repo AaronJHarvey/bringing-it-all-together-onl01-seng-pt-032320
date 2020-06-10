a5ab8e88e9ff96ce121fd728a1fa71992d20bdb3
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-06-10 02:34:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,7 @@ class Dog
         dog_data = dog[0]
         dog = Dog.new(id:dog_data[0],name:dog_data[1],breed:dog_data[2])
       else
-        dog.create(name, breed)
+        dog.create(name:name, breed:breed)
       end
       dog
     end
