c86418c4e8090337027717067ae15fe4ddcb82c7
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-06-10 02:07:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,6 +47,6 @@ class Dog
       @id = row[0]
       @name = row[1]
       @breed = row[2]
-      self.new(name:name,breed:breed, id:id)
+      self.new(name:name, breed:breed, id:id)
     end
 end
