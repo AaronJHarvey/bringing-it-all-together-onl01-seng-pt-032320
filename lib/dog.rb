049584a595c6577f65de66190531a0e710f8b15a
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-06-10 02:07:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,9 +44,9 @@ class Dog
     end
     
     def self.new_from_db(row)
-      @id = row[0]
-      @name = row[1]
-      @breed = row[2]
+      id = row[0]
+      name = row[1]
+      breed = row[2]
       self.new(name:name, breed:breed, id:id)
     end
 end
